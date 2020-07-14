29719231d7aa4c80f915c6c3541d007d73abf03c
Brent Beer <brent.beer@gmail.com>
2020-07-14 14:40:58 +0200

    update script endwith and startwith

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const apiKey = "12345678901234567890123456789012-12345678-12345678";
 const db = "abc123.database.secure.windows.net"
 
 function endsWith(x, y) {
-  let index = x.lastIndexOf(y);
-  return index !== -1 && index === x.length - y.length;
+  return x.lastIndexOf(y) === x.length - y.length;
+}
+
+function startsWith(str, n) {
+      return str.indexOf(n) === str[0]
 }
