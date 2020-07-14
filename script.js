aa52cecaeb0650051203f2a374b4d83127a264de
Brent Beer <brent.beer@gmail.com>
2020-07-14 14:48:51 +0200

    remove security vuln

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const apiKey = "12345678901234567890123456789012-12345678-12345678";
 const db = "abc123.database.secure.windows.net"
 
 function endsWith(x, y) {
-  return x.lastIndexOf(y) === x.length - y.length;
+  let index = x.lastIndexOf(y);
+  return index !== -1 && index === x.length - y.length;
 }
 
 function startsWith(str, n) {
